Removed the optimization of the linear phase analysis and calculate the displacement and force vectors per phase and not at the beginning for everything. Did this because it resulted in wrong results.
Added a new test for the linear phase analysis, which tests the case that frame are added in phases.

pystructural/tests/test_pystructural_results.py
@@ -6,8 +6,9 @@ import numpy as np
 # BASIC RESULT TESTS #
 ######################
 
-# Tests a structure with a simple point load in the middle of the frame
 def test_basic_result_0():
+    """Tests a structure with a simple point load in the middle of the frame
+    """
     # Create a structure instance
     structure = ps.core.Structure2D()
     # Add a frame element
@@ -25,8 +26,9 @@ def test_basic_result_0():
     assert np.allclose(structure.get_point_displacement_vector([5.0, 0.0]), np.array([0.0, -1000 / 48, 0.0]))
 
 
-# Tests a structure with a simple q-load over the whole frame
 def test_basic_result_1():
+    """Tests a structure with a simple q-load over the whole frame
+    """
     # Create a structure instance
     structure = ps.core.Structure2D(0.05)
     # Add a frame element
@@ -48,8 +50,9 @@ def test_basic_result_1():
 # LOAD COMBINATION RESULT TESTS #
 #################################
 
-# Tests a structure with two line q-loads which are combined in a load combination
 def test_load_combination_result_0():
+    """Tests a structure with two line q-loads which are combined in a load combination
+    """
     # Create a structure instance
     structure = ps.core.Structure2D()
     # Add a frame element
@@ -77,6 +80,8 @@ def test_load_combination_result_0():
 ################################
 
 def test_phased_analysis_result_0():
+    """Tests a structure that consists of one frame with two additive linear phases
+    """
     # Create the phased analysis
     phase_analysis = ps.solver.PhasedAnalysis()
     # Create the two phases
@@ -85,7 +90,7 @@ def test_phased_analysis_result_0():
     # Add phase_0 as the previous phase to phase_1
     phase_analysis.add_previous_phase(phase_1, phase_0)
     # Create a structure instance
-    structure = ps.core.Structure2D(0.05)
+    structure = ps.core.Structure2D()
     structure.set_phase(phase_0, phase_1)
     # Add a frame element
     frame_id = structure.add_frame_element([0.0, 0.0], [10.0, 0.0], 1.0, 1.0, 1.0, 1.0)
@@ -104,3 +109,57 @@ def test_phased_analysis_result_0():
     # Test the displacements in the middle of the frame
     assert np.allclose(structure.get_point_displacement_vector([5.0, 0.0]),
                        np.array([0.0, -1000 / 48 + -50000 / 384, 0.0]))
+
+
+def test_phased_analysis_result_1():
+    """Tests a structure that consists of two frame which are static undetermined with two additive linear phases.
+    Tests specifically if adding and removing frames in phases doesn't break the code.
+    """
+    # Create the phased analysis
+    phase_analysis = ps.solver.PhasedAnalysis()
+    # Create the two phases
+    phase_0 = phase_analysis.add_phase('phase_0')
+    phase_1 = phase_analysis.add_phase('phase_1')
+    phase_2 = phase_analysis.add_phase('phase_2')
+    # Add phase_0 as the previous phase to phase_1
+    phase_analysis.add_previous_phase(phase_2, phase_0, phase_1)
+    # Create a structure instance
+    structure = ps.core.Structure2D(0.05)
+
+    # Add a frame element
+    structure.set_phase(phase_0, phase_2)
+    frame_id_0 = structure.add_frame_element([0.0, 0.0], [5.0, 0.0], 1.0, 1.0, 1.0, 1.0)
+    structure.set_phase(phase_1, phase_2)
+    frame_id_1 = structure.add_frame_element([5.0, 0.0], [10.0, 0.0], 1.0, 1.0, 1.0, 1.0)
+
+    # Phase 0
+    structure.set_phase(phase_0)
+    # Add supports
+    structure.add_support([0.0, 0.0], displacement_x=False, displacement_y=False)
+    structure.add_support([5.0, 0.0], displacement_y=False)
+    # Add a q-load
+    structure.add_global_q_load(frame_id_0, -1.0)
+
+    # Phase 1
+    structure.set_phase(phase_1)
+    # Add supports
+    structure.add_support([5.0, 0.0], displacement_x=False, displacement_y=False)
+    structure.add_support([10.0, 0.0], displacement_y=False)
+    # Add a q-load
+    structure.add_global_q_load(frame_id_1, -1.0)
+
+    # Phase 2
+    structure.set_phase(phase_2)
+    # Add supports
+    structure.add_support([0.0, 0.0], displacement_y=False)
+    structure.add_support([5.0, 0.0], displacement_x=False, displacement_y=False)
+    structure.add_support([10.0, 0.0], displacement_y=False)
+    # Add a q-load
+    structure.add_global_q_load(frame_id_0, -1.0)
+    structure.add_global_q_load(frame_id_1, -1.0)
+
+    # Solve the linear system
+    structure.solve_linear_phase_system(phase_analysis)
+
+    # Test the forces at the middle support in the frames
+    assert np.allclose(structure.get_line_force_vector([4.99, 0.0])[3:], np.array([0.0, 5.525, 3.1233]), rtol=1.e-4)