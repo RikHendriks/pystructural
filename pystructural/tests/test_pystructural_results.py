7007215898cb0b0977779c43f67418119cbad48d
Rik Hendriks <rikhendriks@rocketmail.com>
2018-07-29 11:36:45 +0200

    Added a test to the pystructural_results tests in which an imposed load is used.

--- pystructural/tests/test_pystructural_results.py
+++ pystructural/tests/test_pystructural_results.py
@@ -7,7 +7,7 @@ import numpy as np
 ######################
 
 def test_basic_result_0():
-    """Tests a structure with a simple point load in the middle of the frame
+    """Tests a structure with a simple point load in the middle of the frame.
     """
     # Create a structure instance
     structure = ps.core.Structure2D()
@@ -27,7 +27,7 @@ def test_basic_result_0():
 
 
 def test_basic_result_1():
-    """Tests a structure with a simple q-load over the whole frame
+    """Tests a structure with a simple q-load over the whole frame.
     """
     # Create a structure instance
     structure = ps.core.Structure2D(0.05)
@@ -46,12 +46,32 @@ def test_basic_result_1():
     assert np.allclose(structure.get_point_displacement_vector([5.0, 0.0]), np.array([0.0, -50000 / 384, 0.0]))
 
 
+def test_basic_result_2():
+    """Tests a structure with an imposed load over the whole frame.
+    """
+    # Create a structure instance
+    structure = ps.core.Structure2D(0.05)
+    # Add a frame element
+    frame_id = structure.add_frame_element([0.0, 0.0], [10.0, 0.0], 1.0, 1.0, 1.0, 1.0)
+    # Add supports
+    structure.add_support([0.0, 0.0], displacement_x=False, displacement_y=False)
+    structure.add_support([10.0, 0.0], displacement_y=False)
+    # Add an imposed load
+    structure.add_imposed_load(frame_id, [0.0, 100.0])
+    # Solve the linear system
+    structure.solve_linear_system()
+    # Test the forces in the middle of the frame
+    assert np.allclose(structure.get_line_force_vector([4.99, 0.0])[3:], np.array([0.0, 0.0, 0.0]), rtol=1.e4)
+    # Test the displacements in the middle of the frame
+    assert np.allclose(structure.get_point_displacement_vector([5.0, 0.0]), np.array([0.0, -1.25e3, 0.0]))
+
+
 #################################
 # LOAD COMBINATION RESULT TESTS #
 #################################
 
 def test_load_combination_result_0():
-    """Tests a structure with two line q-loads which are combined in a load combination
+    """Tests a structure with two line q-loads which are combined in a load combination.
     """
     # Create a structure instance
     structure = ps.core.Structure2D()
@@ -80,7 +100,7 @@ def test_load_combination_result_0():
 ################################
 
 def test_phased_analysis_result_0():
-    """Tests a structure that consists of one frame with two additive linear phases
+    """Tests a structure that consists of one frame with two additive linear phases.
     """
     # Create the phased analysis
     phase_analysis = ps.solver.PhasedAnalysis()
